5af4d67db521e21f33d86be419f08aeb834ecc03
A3L <krsolucoesweb@gmail.com>
2015-03-29 20:38:42 -0300

    new update
    version 1.0.0.0

---
+++ AutoUpdater/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DownloadButton = New System.Windows.Forms.Button()
+        Me.MyVersionL = New System.Windows.Forms.Label()
+        Me.LatestVersionL = New System.Windows.Forms.Label()
+        Me.loadingbar = New System.Windows.Forms.ProgressBar()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(206, 51)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Settings"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(22, 29)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(158, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Check for update on startup"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Launcher Version : " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Update Version : "
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DownloadButton)
+        Me.GroupBox2.Controls.Add(Me.MyVersionL)
+        Me.GroupBox2.Controls.Add(Me.LatestVersionL)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 97)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(206, 78)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Version information"
+        '
+        'DownloadButton
+        '
+        Me.DownloadButton.Location = New System.Drawing.Point(22, 55)
+        Me.DownloadButton.Name = "DownloadButton"
+        Me.DownloadButton.Size = New System.Drawing.Size(134, 23)
+        Me.DownloadButton.TabIndex = 4
+        Me.DownloadButton.Text = "Download"
+        Me.DownloadButton.UseVisualStyleBackColor = True
+        Me.DownloadButton.Visible = False
+        '
+        'MyVersionL
+        '
+        Me.MyVersionL.AutoSize = True
+        Me.MyVersionL.Location = New System.Drawing.Point(105, 38)
+        Me.MyVersionL.Name = "MyVersionL"
+        Me.MyVersionL.Size = New System.Drawing.Size(48, 13)
+        Me.MyVersionL.TabIndex = 3
+        Me.MyVersionL.Text = "LATEST"
+        '
+        'LatestVersionL
+        '
+        Me.LatestVersionL.AutoSize = True
+        Me.LatestVersionL.Location = New System.Drawing.Point(114, 25)
+        Me.LatestVersionL.Name = "LatestVersionL"
+        Me.LatestVersionL.Size = New System.Drawing.Size(48, 13)
+        Me.LatestVersionL.TabIndex = 2
+        Me.LatestVersionL.Text = "LATEST"
+        '
+        'loadingbar
+        '
+        Me.loadingbar.Location = New System.Drawing.Point(12, 69)
+        Me.loadingbar.Name = "loadingbar"
+        Me.loadingbar.Size = New System.Drawing.Size(206, 23)
+        Me.loadingbar.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.loadingbar.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(227, 197)
+        Me.Controls.Add(Me.loadingbar)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Updating"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents MyVersionL As System.Windows.Forms.Label
+    Friend WithEvents LatestVersionL As System.Windows.Forms.Label
+    Friend WithEvents DownloadButton As System.Windows.Forms.Button
+    Friend WithEvents loadingbar As System.Windows.Forms.ProgressBar
+
+End Class
---
+++ AutoUpdater/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports System.Net
+Imports System.ComponentModel
+
+Public Class Form1
+    Dim autoupdate As String = My.Settings.CheckForUpdate
+    Dim latestversion As String = "1.0.0.0"
+    Dim downlink As String
+    Dim vlauncher As String = "http://pastebin.com/raw.php?i=FgUgEfMt"
+    Dim vdownload As String = "http://pastebin.com/raw.php?i=TkYj2SdZ"
+    Dim servername = "AltisDEV"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If autoupdate = "True" Then
+            Dim Str As System.IO.Stream
+            Dim srRead As System.IO.StreamReader
+            Dim Str2 As System.IO.Stream
+            Dim srRead2 As System.IO.StreamReader
+
+            Try
+
+                Dim req As System.Net.WebRequest = System.Net.WebRequest.Create(vlauncher)
+                Dim resp As System.Net.WebResponse = req.GetResponse
+                Str = resp.GetResponseStream
+                srRead = New System.IO.StreamReader(Str)
+
+                latestversion = srRead.ReadToEnd
+                My.Settings.LatestVersion = latestversion
+            Catch ex As Exception
+                MsgBox("Could recieve latest version!" & vbNewLine & " Please check your internet connection!", MsgBoxStyle.Information, "Auto Update")
+
+            Finally
+
+            End Try
+
+            Try
+
+                Dim req2 As System.Net.WebRequest = System.Net.WebRequest.Create(vdownload)
+                Dim resp2 As System.Net.WebResponse = req2.GetResponse
+                Str2 = resp2.GetResponseStream
+                srRead2 = New System.IO.StreamReader(Str2)
+
+                downlink = srRead2.ReadToEnd
+            Catch ex As Exception
+
+            End Try
+        Else
+        End If
+
+
+
+        LatestVersionL.Text = latestversion
+        MyVersionL.Text = "1.0.0.0"
+
+        If LatestVersionL.Text = "1.0.0.0" Then
+            MyVersionL.ForeColor = Color.Green
+        Else
+            MsgBox("Update available!", MsgBoxStyle.Information, "Auto Update")
+            MyVersionL.ForeColor = Color.Red
+            DownloadButton.Visible = True
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            My.Settings.CheckForUpdate = "True"
+            My.Settings.Save()
+        Else
+            My.Settings.CheckForUpdate = "False"
+            My.Settings.Save()
+        End If
+    End Sub
+
+    Private Sub DownloadButton_Click(sender As Object, e As EventArgs) Handles DownloadButton.Click
+        Dim client As WebClient = New WebClient
+        AddHandler client.DownloadProgressChanged, AddressOf Me.client_DownloadProgressChanged
+        AddHandler client.DownloadFileCompleted, AddressOf Me.client_DownloadFileCompleted
+        ' Starts the download
+        client.DownloadFileAsync(New Uri(downlink), Application.StartupPath + "/" & servername & ".exe")
+        DownloadButton.Text = "Download In Process"
+        DownloadButton.Enabled = False
+    End Sub
+
+    Private Sub DownloadLink_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs)
+
+    End Sub
+
+    Private Sub client_DownloadProgressChanged(ByVal sender As Object, ByVal e As DownloadProgressChangedEventArgs)
+        Dim bytesIn As Double = Double.Parse(e.BytesReceived.ToString)
+        Dim totalBytes As Double = Double.Parse(e.TotalBytesToReceive.ToString)
+        Dim percentage As Double = (bytesIn / (totalBytes * 100))
+        If loadingbar.Value = 100 Then
+            loadingbar.Value = loadingbar.Value - 1
+        Else
+            loadingbar.Value = loadingbar.Value + 1
+        End If
+    End Sub
+
+    Private Sub client_DownloadFileCompleted(ByVal sender As Object, ByVal e As AsyncCompletedEventArgs)
+        loadingbar.Value = loadingbar.Value + 1
+        MsgBox("Download Completed", MsgBoxStyle.Information, "Auto Update")
+        DownloadButton.Text = "Start Download"
+        DownloadButton.Enabled = True
+        Application.Exit()
+        Process.Start("AltisDEV.exe")
+    End Sub
+End Class
